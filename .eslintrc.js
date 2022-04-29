e1ebfe22b572573b4e7d6e46d229dd74c23f119f
Taylor Roberts <taylorlroberts7@gmail.com>
2022-04-29 16:25:46 -0600

    chore: add semantic release

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  rules: {
+    "import/order": [
+      "error",
+      {
+        alphabetize: {
+          caseInsensitive: false,
+          order: "asc",
+        },
+        groups: [
+          "object",
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+      },
+    ],
+  },
+};
